3c71f9d2239e32eb71f26d30136b634e526bb3ea
Isaiah Jackson <isaiah.t2398@gmail.com>
2023-10-04 06:26:48 -0700

    My Personal Website

---
+++ javascript.js
@@ -0,0 +1,56 @@
+// targetPage.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get a reference to the button element by its ID
+    var redirectButton = document.getElementById("Home");
+
+    // Add a click event listener to the button
+    redirectButton.addEventListener("click", function () {
+        // Redirect to the desired HTML page
+        window.location.href = "index.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    // Get a reference to the button element by its ID
+    var redirectButton = document.getElementById("About");
+
+    // Add a click event listener to the button
+    redirectButton.addEventListener("click", function () {
+        // Redirect to the desired HTML page
+        window.location.href = "about.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    // Get a reference to the button element by its ID
+    var redirectButton = document.getElementById("Portfolio");
+
+    // Add a click event listener to the button
+    redirectButton.addEventListener("click", function () {
+        // Redirect to the desired HTML page
+        window.location.href = "portfolio.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    // Get a reference to the button element by its ID
+    var redirectButton = document.getElementById("Contact");
+
+    // Add a click event listener to the button
+    redirectButton.addEventListener("click", function () {
+        // Redirect to the desired HTML page
+        window.location.href = "contact.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    // Get a reference to the button element by its ID
+    var redirectButton = document.getElementById("submit");
+
+    // Add a click event listener to the button
+    redirectButton.addEventListener("click", function () {
+        // Redirect to the desired HTML page
+        window.location.href = "submission.html"
+    });
+});
+
+
+
+
